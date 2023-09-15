f84624483030bee98cce2cc1dbf9298d9ed08b8b
Jeff Zabel <jeff.zabel@gmail.com>
2023-09-14 20:56:26 -0600

    Updated to use scheduler

--- run_db_monitoring.py
+++ run_db_monitoring.py
@@ -1,20 +1,36 @@
-import smbus
-
-
-def report_db_level():
-	device_bus = 1
-	device_addr = 0x48
-	bus_handler = smbus.SMBus(device_bus)
-	recorded_db = bus_handler.read_byte_data(device_addr, 0x0A)
-	current_timestamp = str(datetime.datetime.now())
-
-	return current_timestamp, recorded_db
-
+import time
+import datetime
+import sched
+import pigpio
 
 def main():
-	for i in range(100):
-		current_timestamp, recorded_db = report_db_level()
-		print("{}|{}".format(current_timestamp, recorded_db))
+
+	#Entering main script
+	print("Starting up")
+
+
+	#Enable reading of bus and create handle
+	pi = pigpio.pi()
+	h = pi.i2c_open(1, 0x48) #open device at address 0x48 on bus 1
+	#Try a first read
+	first_db = pi.i2c_read_byte_data(h, 0x0A)
+	print ("First db is" + str(first_db))
+
+
+	#Set up scheduler to read each second
+	def read_db(scheduler):
+		#schedule the first call
+		scheduler.enter(1, 1, read_db, (scheduler,))
+		#Execute the sound buffer reads
+		db = pi.i2c_read_byte_data(h, 0x0A)
+		print ("db is " + str(db) + ' @ ' + str(datetime.datetime.now()))
+		return db
+
+	#Execute/schedule 1 second sound readings
+	scheduled_sound_read=sched.scheduler(time.time, time.sleep)
+	scheduled_sound_read.enter(1, 1, read_db, (scheduled_sound_read,))
+	print("Start reading sound levels")
+	scheduled_sound_read.run()
 
 
 if __name__ == "__main__":
